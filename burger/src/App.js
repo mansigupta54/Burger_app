696e71357825b2c9f6cfedfeaa6f9be8d037dd54
vsc009 <imvishalchauhan13@gmail.com>
2021-04-16 23:37:03 +0530

    final project

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,15 +1,18 @@
-import './App.css';
+import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <p>Hell is Here</p>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
